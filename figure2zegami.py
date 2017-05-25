4f6b6862ad5c4072c85dc374a63a2b51e105718b
Stephen Taylor <stephen.taylor@imm.ox.ac.uk>
2017-05-25 09:53:10 +0100

    hostname, username and password now passed via environment variables to help security.

--- figure2zegami.py
+++ figure2zegami.py
@@ -2,13 +2,14 @@
 from omero.rtypes import wrap
 from omero.gateway import BlitzGateway
 import omero
-import os.path
+import os
 import csv
 
-USERNAME = "xxx"
-PASSWORD = "xxx"
+USERNAME = os.environ["USER"]
+PASSWORD = os.environ["PASS"]
+HOST = os.environ["HOST"]
 
-conn = BlitzGateway(USERNAME, PASSWORD, host="xxx", port=4064)
+conn = BlitzGateway(USERNAME, PASSWORD, host=HOST, port=4064)
 conn.connect()
 
 # cross-group query to find file
@@ -18,17 +19,19 @@ SCRIPT_PATH = "/omero/figure_scripts/Figure_To_Pdf.py"
 
 
 #id = 2086989;
+#to check figures
+
 format = 'PDF'
 zegami_tsv = "zegami.tsv"
 y = open(zegami_tsv,'w')
 z = csv.writer(y, delimiter="\t")
 
 # write the header
-z.writerow(["gene","bangalore","probe", "author","image","figure_id"])
+z.writerow(["gene","bangalore","probe", "author","pdf","figure_id","path"])
 
 script_service = conn.getScriptService()
 script_id = script_service.getScriptID(SCRIPT_PATH)
-
+num = 1
 
 for f in conn.getObjects("FileAnnotation", attributes={"ns": "omero.web.figure.json"}):
     # name of figure using naming convention agreed and then made upper case
@@ -48,21 +51,27 @@ for f in conn.getObjects("FileAnnotation", attributes={"ns": "omero.web.figure.j
     if len(meta_data) == 6:
     	# each one should have a banglore id and a tissue, currently BRAIN for testing
 	# to do: add more filters 
+	
+	# This is a specific template file that is left in
+	if "TEMPLATE" in meta_data[0]:
+		continue
+	
+	
     	if "VNC" in meta_data[2] or "MB" in meta_data[2] or "OL" in meta_data[2] or "CB" in meta_data[2]:
     		print "Checking if "+file_name+" is downloaded."
+		print "Number of files processed = "+ str(num)
 	    	print "id="+ str(id)
   	  	print "0 = "+ str(meta_data)
   	  	print "1 = "+meta_data[1]
 		print "2 = "+meta_data[2]
-
+		# add a column to metadata
+		meta_data.append(figure_name+".png")
     		z.writerow(meta_data)
     
+    		# skip download if exists
     		if os.path.isfile(file_name):
-    			print "Got it already."
+    			print "Got "+file_name+" already."
     			continue
- 		
-		
-		#continue
 		
 		
 		figure_ann = conn.getObject("FileAnnotation", id)
@@ -104,4 +113,16 @@ for f in conn.getObjects("FileAnnotation", attributes={"ns": "omero.web.figure.j
   			finally:
        				f.close()
        				print "File ",file_name,"downloaded!"
+		
+
+		# create a png
+		os.system('mogrify -density 400 -background white -alpha remove -format png '+file_name)
+		num = num + 1
+
+
+# Final processing to remove all extraneous pngs
+# could probably be done better in python
+os.system('find . -name "*\-[12345679].png" -exec rm {} \;') 
+os.system('rename "s/-0//" *.png')
+		
 
